ac83a2afc7702e49d7713d2592653c27ca50eccd
Komesu YU <e215742@ie.u-ryukyu.ac.jp>
2023-05-11 13:07:47 +0900

    generte ex1.1.png

--- dataset1.py
+++ dataset1.py
@@ -8,11 +8,21 @@ font_path = '/Library/Fonts/Arial Unicode.ttf'
 font_prop = font_manager.FontProperties(fname = font_path)
 matplotlib.rcParams['font.family'] = font_prop.get_name()
 
-x=np.zeros(1, dtype=float)
+x=np.linspace(-1, 1, 100)
 def true_function(x):
     y = sin(pi * x * 0.8) * 10
     return y
     
 plt.figure(0)
-plt.plot(x, true_function(x))
+plt.plot(x, true_function(x), label='true_function')
+plt.xlabel('x')
+plt.ylabel('true_function')
+plt.grid(True)
+
+plt.legend()
+
 plt.show()
+
+
+plt.savefig('ex1.1.png')
+
