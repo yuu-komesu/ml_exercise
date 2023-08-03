45c9ea934d9528888854ca6962ac82f10474c67c
Komesu YU <e215742@ie.u-ryukyu.ac.jp>
2023-08-03 16:09:03 +0900

    update test_dataset1.py

--- test_dataset1.py
+++ test_dataset1.py
@@ -4,11 +4,11 @@ import numpy as np
 
 class TestFunctionMethods1(unittest.TestCase):
 	def test_zero(self):
-		x=np.zeros((1,), dtype=float)
+		x=np.linspace(0, 0, 1)
 		self.assertEqual(true_function(x), np.zeros((1,), dtype=float))
 	
 	def test_two_zeros(self):
-		x=np.zeros((2,), dtype=float)
+		x=np.linspace(0, 0, 2)
 		arr1=true_function(x)
 		arr2=np.zeros((2,), dtype=float)
 		self.assertEqual(list(arr1), list(arr2))
